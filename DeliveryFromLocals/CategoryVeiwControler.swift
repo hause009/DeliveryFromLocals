b3a0056e93e0b76ef84bd8c657582fceeb69769f
Alex <hause.abramovi44@gmail.com>
2016-10-11 23:59:25 +0300

    add class

---
+++ DeliveryFromLocals/CategoryVeiwControler.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryVeiwControler.swift
+//  DeliveryFromLocals
+//
+//  Created by Alex on 03.10.16.
+//  Copyright © 2016 AnsA. All rights reserved.
+//
+
+//import Cocoa
+import UIKit
+let nameString2 = "11"
+
+class CategoryVeiwControler: UIViewController {
+    
+    var nameString = ""
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(nameString)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
