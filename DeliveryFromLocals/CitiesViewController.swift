2dae7d94a92e7d81797d046fb91277be81088a66
Alex <hause.abramovi44@gmail.com>
2016-10-11 23:57:19 +0300

    fest commit

---
+++ DeliveryFromLocals/CitiesViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  DeliveryFromLocals
+//
+//  Created by Alex on 03.10.16.
+//  Copyright © 2016 AnsA. All rights reserved.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+    //SegueToCategor SegueToSpb
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SegueToSpb"
+        {
+            let vc = segue.destination as! CategoryVeiwControler
+            vc.title = "Санкт Петербург"
+            vc.nameString = "SPb"
+        }
+        else if segue.identifier == "SegueToMoscov"{
+            let vc = segue.destination as! CategoryVeiwControler
+            vc.title = "Москва"
+            vc.nameString = "Moscov"
+        }
+        else if segue.identifier == "SegueToKazan"{
+            let vc = segue.destination as! CategoryVeiwControler
+            vc.title = "Казань"
+            vc.nameString = "Kazan"
+        }
+        else if segue.identifier == "SegueToEkaterinburg"{
+            let vc = segue.destination as! CategoryVeiwControler
+            vc.title = "Екатеринбург"
+            vc.nameString = "Ekaterinburg"
+        }
+    }
+    
+    @IBAction func pressButtonMoscov(_ sender: AnyObject) {
+
+        performSegue(withIdentifier: "SegueToMoscov", sender: 1)
+    }
+   
+    @IBAction func pressButtonKazan(_ sender: AnyObject) {
+        
+        performSegue(withIdentifier: "SegueToKazan", sender: 2)
+    }
+    @IBAction func pressButtonEkaterinburg(_ sender: AnyObject) {
+        
+        performSegue(withIdentifier: "SegueToEkaterinburg", sender: 3)
+    }
+    
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
--- DeliveryFromLocals/FoodViewController.swift
+++ DeliveryFromLocals/FoodViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  FoodViewController.swift
 //  DeliveryFromLocals
 //
 //  Created by Alex on 03.10.16.
 //  Copyright © 2016 AnsA. All rights reserved.
 //
 
+//import Cocoa
 import UIKit
 
-class ViewController: UIViewController {
-
+class FoodViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
